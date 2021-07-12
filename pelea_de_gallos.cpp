a8d3c7ff4e4eb29c715a86d60b2abe51843629b0
Andres-brek <69656172+Andres-brek@users.noreply.github.com>
2021-07-11 22:29:48 -0500

    Update 0.3v
    JAPA

--- pelea_de_gallos.cpp
+++ pelea_de_gallos.cpp
@@ -76,26 +76,32 @@ class Gallos{
 			}
         }
 		
-		void Buscar(string nombre_buscado){
+		Nodo* Buscar(string nombre_buscado){
 			if(!listavacia()){
 				Nodo *p;
 				p=Ptr;
-				int C=0;
+				Nodo *C=Ptr;
+				bool encontrado=true;
 				while(p->Datos.Nombre.compare(nombre_buscado)!=0)
 				{
 					p=p->siguiente;
-					C++;
-					if(C==100){
+					if(C==p){
+						cout<<"\nEl gallo buscado no se encuentra";
+						encontrado=false;
 						break;
 					}
 				}
-				cout<<"\nNombre del gallo: "<<p->Datos.Nombre<<"\n Altura: "<<p->Datos.Altura<<"\n Edad: "<<p->Datos.Edad<<"\n Peso: "<<p->Datos.Peso<<"\n Raza: "<<p->Datos.Raza<<"\n Victorias: "<<p->Datos.Victorias<<"\n Derrotas: "<<p->Datos.Derrotas;
+				
+				if(encontrado)
+				{
+					return p; 
+				}
+				
 			}
 			else{
 				cout<<"\nLista Vacia";
 			}
 		}
-		
 		void eliminarx(string nombre_buscado){
 			if(!listavacia()){
 				Nodo *p;
@@ -121,7 +127,7 @@ class Gallos{
 				{
 					Ptr=NULL;
 					Ult=NULL;
-					delete z,q,p;
+					p=NULL;
 				}
 				else if(q==Ptr)
 				{
@@ -138,6 +144,8 @@ class Gallos{
 				cout<<"\nLista Vacia";
 			}
 		}
+		
+		
 		
 		bool listavacia(){
 			if(Ptr==NULL){
@@ -156,26 +164,14 @@ class Gallos{
     };
 int main(){
 	Gallo Gallito;
-	Gallos Gallod;
-	Gallito.Nombre="Andres";
-	Gallito.Altura=15;
-	Gallito.Derrotas=2;
-	Gallito.Edad=5;
-	Gallito.Peso=5;
-	Gallito.Raza="Chandita";
-	Gallito.Victorias=1;
-	Gallod.Agregar_inicio(Gallito);
-	Gallod.mostrar();
-	Gallod.Buscar("Andres");
-	Gallod.eliminarx("Andres");
-	Gallod.mostrar();
+    Gallos Gallod;
 	int opc=0;
-    	int c=1;
-    	while (c==1){
+    int c=1;
+    while (c==1){
 		cout<<"====================\n";
 		cout<<"===Menu Principal===\n";
 		cout<<"====================\n";
-		cout<<"1. Agregar gallo: \n";
+		cout<<"1. Agregar gallo al inicio: \n";
 		cout<<"2. Mostrar lista de gallos \n";
 		cout<<"3. Eliminar gallo \n";
 		cout<<"4. Modificar datos \n";
@@ -190,18 +186,67 @@ int main(){
 		    case 1:
 			{
 			    cout<<"Ingrese el nombre del gallo: ";
+			    string Nombre_nuevo;
+			    cin>>Nombre_nuevo;
+			    cout<<"Ingrese la altura del gallo (cm): ";
+			    float Altura_nueva;
+			    cin>>Altura_nueva;
+			    cout<<"Ingrese la edad del gallo (Meses): ";
+			    int Edad_nueva;
+			    cin>>Edad_nueva;
+			    cout<<"Ingrese el peso del gallo (Kg): ";
+			    float peso_nuevo;
+			    cin>>peso_nuevo;
+			    cout<<"Ingrese la raza del gallo: ";
+			    string raza_nueva;
+			    cin>>raza_nueva;
+			    cout<<"Ingrese las victorias del gallo: ";
+			    int victorias_nueva;
+			    cin>>victorias_nueva;
+			    cout<<"Ingrese las derrotas del gallo: ";
+			    int derrotas_nueva;
+			    cin>>derrotas_nueva;
+				Gallito.Victorias=victorias_nueva;
+			    Gallito.Derrotas=derrotas_nueva;
+			    Gallito.Raza=raza_nueva;
+			    Gallito.Peso=peso_nuevo;
+			    Gallito.Altura=Altura_nueva;
+			    Gallito.Nombre=Nombre_nuevo;
+			    Gallito.Edad=Edad_nueva;
+			    Gallod.Agregar_inicio(Gallito);
 			}
 		    break;
 		    case 2:
 				Gallod.mostrar();
 		    break;
-		    case 3:
+		    case 3:{
+		    	
+				if(!Gallod.listavacia())
+				{
+					cout<<"Ingrese el nombre del gallo que desea buscar: ";
+		    		string nombre;
+		    		cin>>nombre;
+					Gallod.eliminarx(nombre);
+					cout<<"\n Eliminado con exito";
+					Gallod.mostrar();
+				}
+				else{
+					cout<<"Lista Vacia";
+				}
+				
+
+			}
 		    break;
 		    case 4:
 		    break;
-		    case 5:
+		    case 5:{
+			
+		    	cout<<"Ingrese el nombre del gallo que desea buscar: ";
+		    	string nombre;
+		    	cin>>nombre;
+		    	Nodo *gallo=Gallod.Buscar(nombre);
+		    	cout<<"\nNombre del gallo: "<<gallo->Datos.Nombre<<"\n Altura: "<<gallo->Datos.Altura<<"\n Edad: "<<gallo->Datos.Edad<<"\n Peso: "<<gallo->Datos.Peso<<"\n Raza: "<<gallo->Datos.Raza<<"\n Victorias: "<<gallo->Datos.Victorias<<"\n Derrotas: "<<gallo->Datos.Derrotas;}
 		    break;
-
 		    case 6:
 			cout<<"Fin del programa";
 			c+=1;
